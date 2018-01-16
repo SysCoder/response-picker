1fc456a5abca2d3b8063557ae19a724221ccedf9
Marcellus Pelcher <pelcher@gmail.com>
2018-01-16 03:06:36 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,65 @@
+'use strict';
+var hashGen = require("shorthash");
+
+class RepsonsePicker {
+  constructor(trackingObject) {
+    this.trackingObject = trackingObject;
+  }
+  
+  pickResponse(prompts) {
+    let promptHash = hashGen.unique(prompts.toString());
+    
+    // Initialize data object for list of prompts.
+    if (this.trackingObject[promptHash] === undefined) {
+      let promptTrackingData = new Array(prompts.length).fill(0);
+      this.trackingObject[promptHash] = {
+        promptTrackingData: promptTrackingData,
+        lastPromptLocation: promptTrackingData,
+      };
+    }
+    
+    let promptTrackingData = this.trackingObject[promptHash]["promptTrackingData"];
+    let lastPromptLocation = this.trackingObject[promptHash]["lastPromptLocation"];
+    
+    let filterFromPrompt = undefined;
+    let prompt = undefined;
+    // Reset tracking data once all prompts have been used.
+    // Exclude the last prompt index so repeats will not happen.
+    if (promptTrackingData.indexOf(0) === -1) {
+      // Pick any element except the one that was picked last
+      prompt = getRandomPromptUsingFilter(prompts, lastPromptLocation);
+      filterFromPrompt = addPromptIndexToNewFilter(prompt, prompts);
+      promptTrackingData.fill(0);
+      promptTrackingData = unionFilters(promptTrackingData, filterFromPrompt);
+    } else {
+      prompt = getRandomPromptUsingFilter(prompts, promptTrackingData);
+      filterFromPrompt = addPromptIndexToNewFilter(prompt, prompts);
+      promptTrackingData = unionFilters(promptTrackingData, filterFromPrompt);
+    }
+    
+    this.trackingObject[promptHash] = {
+      promptTrackingData: promptTrackingData,
+      lastPromptLocation: filterFromPrompt,
+    };
+    return prompt;
+  }
+
+  getTrackingObject() {
+    return this.trackingObject;
+  }
+}
+
+function getRandomPromptUsingFilter(prompts, filter) {
+  let filteredPrompts = prompts.filter((x, i) => !filter[i]);
+  return filteredPrompts[Math.floor(Math.random() * (filteredPrompts.length))];
+}
+
+function addPromptIndexToNewFilter(prompt, prompts) {
+  return prompts.map(x => (x === prompt) ? 1 : 0);
+}
+
+function unionFilters(firstFilter, secondFilter) {
+  return firstFilter.map((x, i) => (x || secondFilter[i]) ? 1 : 0);
+}
+
+exports.RepsonsePicker = RepsonsePicker;
---
+++ spec/response-picker-spec.js
@@ -0,0 +1,102 @@
+describe("Response Picker", function() {
+  var RepsonsePicker = require('../index').RepsonsePicker;
+
+  it("should not repeat responses",
+    function() {
+     let trackingObject = {};
+     let responsePool = [`Hello world`, `what is the color`, `computer`,
+         `meltdown`, `spectre`, `computer chips`, `processor`,
+         `virus`, `hacker`, `vulnerability`];
+     let responsePicker = new RepsonsePicker(trackingObject);
+     let responses = [];
+     
+     for(let i = 0;i < 10;i++) {
+        responses.push(responsePicker.pickResponse(responsePool));
+     }
+
+     expect(countMaxDuplicates(responses)).toEqual(0);
+  });
+  
+  it("should repeat all responses at most 5 times with a 4 element array and invoked 20 times",
+    function() {
+     let trackingObject = {};
+     let responsePool = [`Hello world`, `what is the color`, `computer`,
+         `meltdown`];
+     let responsePicker = new RepsonsePicker(trackingObject);
+     let responses = [];
+     
+     for(let i = 0;i < 100;i++) {
+        responses.push(responsePicker.pickResponse(responsePool));
+     }
+     expect(countMaxDuplicates(responses)).toEqual(24);
+  });
+  
+  it("should keep track of two response lists and not repeat responses from both list",
+    function() {
+     let trackingObject = {};
+     let responsePoolHello = [`Hello there`, `Hello`, `Hi`, `Howdy`, `Hey`];
+     let responsePoolGoodbye = [`Goodbye`, `Bye`, `Bye Bye`, `See you later!`,
+         `Chao`];
+     let responsePicker = new RepsonsePicker(trackingObject);
+     let helloResponses = [];
+     let goodbyeResponses = [];
+     
+     for(let i = 0;i < 5;i++) {
+        helloResponses.push(responsePicker.pickResponse(responsePoolHello));
+        goodbyeResponses.push(responsePicker.pickResponse(responsePoolGoodbye));
+     }
+     expect(countMaxDuplicates(helloResponses)).toEqual(0);
+     expect(countMaxDuplicates(goodbyeResponses)).toEqual(0);
+  });
+  
+  it("should keep track of responses in different languages",
+    function() {
+     let trackingObject = {};
+     let responsePoolChinese = [`雜碎`, `炒`, `圍棋`, `茄汁`, `唔可以`];
+     let responsePicker = new RepsonsePicker(trackingObject);
+     let chineseResponses = [];
+     
+     for(let i = 0;i < 5;i++) {
+        chineseResponses.push(responsePicker.pickResponse(responsePoolChinese));
+     }
+     expect(countMaxDuplicates(chineseResponses)).toEqual(0);
+  });
+  
+  it("should give the tracking object and be able to use the same object in another RepsonsePicker instance",
+    function() {
+     let trackingObject = {};
+     let responsePoolHello = [`Hello there`, `Hello`, `Hi`, `Howdy`, `Hey`];
+     let responsePicker = new RepsonsePicker(trackingObject);
+     let helloResponses = [];
+     
+     for(let i = 0;i < 4;i++) {
+        helloResponses.push(responsePicker.pickResponse(responsePoolHello));
+     }
+     let newTrackingOjbect = JSON.parse(JSON.stringify(trackingObject));
+     let newResponsePicker = new RepsonsePicker(newTrackingOjbect);
+     
+     let responseFromNewResponsePicker = newResponsePicker.pickResponse(responsePoolHello);
+     let resposneFromOldResponsePicker = responsePicker.pickResponse(responsePoolHello);
+     
+     expect(responseFromNewResponsePicker).toEqual(resposneFromOldResponsePicker);
+  });
+});
+
+function countMaxDuplicates(listOfStrings) {
+    listOfStrings.sort();
+    let maxNumDuplicates = 0;
+    let lastElement = undefined;
+    listOfStrings.reduce((accumulator, currentValue) => {
+        let reVal = 0;
+        if (lastElement === currentValue) {
+            reVal = accumulator + 1;
+            maxNumDuplicates = Math.max(maxNumDuplicates, reVal);
+            lastElement = currentValue;
+            return reVal;
+        }
+        lastElement = currentValue;
+        return reVal;
+    }, 0);
+    return maxNumDuplicates;
+}
+
